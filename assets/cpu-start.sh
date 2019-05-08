113ede381e29955c586235a9fb9f356c3ea8ebe9
Søren Hilmer <sh@amplex.dk>
2019-05-08 14:33:05 +0200

    GL-1931 starts virtual cpu's based on configuration in occustomer table

---
+++ assets/cpu-start.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+###
+# @file cpu-start.sh
+# ABSTRACT HERE << 
+#
+# $Id$
+#
+# (C) Copyright 2018 Amplex, fm@amplex.dk
+#
+WSPASSWD=`cat /opt/amplex/.wspasswd`
+echo "CPU start"
+localhost=localhost
+echo "Y" >> /opt/virtcpu/cpustart
+export PATH=$PATH:/opt/virtcpu/install/root/usr/bin
+grep -s -q linuxkit /proc/version && localhost=host.docker.internal
+
+# Try curl login with "nodes" ws
+curl_mksession() {
+  CODE=$(curl -s --cookie /tmp/ampimp-cookie-jar --cookie-jar /tmp/ampimp-cookie-jar -w '%{http_code}\n' \
+         -H 'Authorization: Basic aW1wb3J0ZXI6SnVtcGluZ0Zpc2hTdGlja0F0MTg=' http://$localhost:8000/aasws/nodes -o /dev/null)
+  [[ $CODE == 200 ]] && echo --cookie /tmp/ampimp-cookie-jar || echo "--uimporter:$WSPASSWD"
+}
+
+lorawan_customers() {
+    LOGIN=$(curl_mksession)
+  curl -s $LOGIN http://$localhost:8000/sso/r/customer > /tmp/customers.json
+  cat /tmp/customers.json | jq -j '.[] | select(.systems) | select(.systems| .[] | .=="amplex.gridlight-lorawan") | "\(.id)\t\(.name)\t\(.systemsConfiguration)\n"'
+}
+
+lorawan_customers | while IFS=$'\t' read id name sysconf ;do
+  srvr=$(amp-param lorawan-driver "server-${name// /-}")
+  if [ -z "$srvr" ] ;then
+    srvr=$(amp-param lorawan-driver server)
+    : ${srvr:=lorawan-server}
+  fi
+  if [ "null" == "$sysconf" ] ;then
+      continue
+  fi
+  spec=`echo $sysconf | jq '.["amplex.gridlight-lorawan"]' `
+#  spec=$(amp-param lorawan-driver "spec-${name// /-}")
+#  if [ -z "$spec" ] ;then
+#    spec='{
+#       "type":"ghp-lorawan-server",
+#       "name":"AmpLoRaWAN-{NAME}",
+#       "rest":"http://{SERVER}/",
+#       "sock":"ws://{SERVER}/",
+#       "customer":{CUST},
+#    }'
+#  fi
+  spec=${spec//'{CUST}'/$id}
+  spec=${spec//'{NAME}'/$name}
+  spec=${spec//'{SERVER}'/$srvr}
+  spec=${spec//$'\n'/}
+  spec=${spec//'"'/'\"'}
+  echo "$spec" > /tmp/lorawan_driver_driver_spec_$id
+  PARAM_lorawan_driver_driver_spec="$spec" virtcpu virtcpu-run $((4002000000 + id)) $localhost &
+done
+
+# vim: set sw=2 sts=2 et:
---
+++ mac-run.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+NAME="virtual-cpu"
+IMAGE="virtual-cpu"
+PORTS="-p 31022:22"
+
+die() {
+  echo "$@"
+  exit 1
+}
+
+common() {
+    if docker ps -q --filter name=$NAME | grep -q ... ;then
+       die "Already running"
+    fi
+    echo "Removing old containers .."
+    docker ps -aq --filter name=$NAME | xargs docker rm -v
+}
+
+start() {
+    common
+    docker run --detach --name $NAME --hostname $NAME $PORTS --volume `pwd`/storage:/amplex $IMAGE
+}
+
+shell() {
+    common
+    docker run --name $NAME --hostname $NAME $PORTS --volume `pwd`/storage:/amplex -it $IMAGE shell
+}
+
+bash() {
+    docker run --name $NAME --volume `pwd`/storage:/amplex -it --entrypoint=/bin/bash $IMAGE -i
+}
+
+stop() {
+    docker ps -q --filter name=$NAME | xargs docker stop
+}
+
+logs() {
+    docker ps -q --filter name=$NAME | xargs docker logs
+}
+
+tail() {
+    docker ps -q --filter name=$NAME | xargs docker logs --follow
+}
+
+build() {
+    common
+    docker build -t ubuntu-stuffed git@github.com:amplexdenmark/ubuntu-stuffed.git
+    docker build -t $IMAGE git@github.com:amplexdenmark/${IMAGE}.git
+    docker image ls $IMAGE
+}
+
+build-local() {
+    common
+    if [ -z "$WSPASSWD" ] ;then
+      echo "Importer password "
+      read -s WSPASSWD
+    else
+      echo "Importer password from environment WSPASSWD"
+    fi
+    docker build -t ubuntu-stuffed git@github.com:amplexdenmark/ubuntu-stuffed.git
+    docker build --build-arg WSPASSWD=$WSPASSWD -t $IMAGE .
+    docker image ls $IMAGE
+}
+
+[ $# -gt 0 ] || die "$0: 'start|stop|logs|tail'"
+mkdir -p storage
+
+"$@"
